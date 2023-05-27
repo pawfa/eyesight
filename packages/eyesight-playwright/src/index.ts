162018de099385585c3e8906e4439d2ac3970bb3
Pawel Fajfer <fajfer.pawel@gmail.com>
2023-05-27 18:19:27 +0200

    Patched log and mkdir, replaced http module with fetch

--- packages/eyesight-playwright/src/index.ts
+++ packages/eyesight-playwright/src/index.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 import http from "http";
 
-fetch('http://localhost:3000/screenshots/example-project/v3').then((res)=>res).then((r)=> r.json()).then((res)=> {
+fetch('http://localhost:3001/screenshots/example-project/v3').then((res)=>res).then((r)=> r.json()).then((res)=> {
     const orygFs= fs.existsSync;
     const orygFsPromiseRead= fs.promises.readFile;
 
@@ -32,13 +32,13 @@ fetch('http://localhost:3000/screenshots/example-project/v3').then((res)=>res).t
     const orygFsWrite= fs.writeFileSync;
 
     (fs.writeFileSync as any) = (...args:any[])=> {
-        if (args[0].endsWith('png') && args[0].indexOf('actual') === -1) {
+        if (args[0].endsWith('png') && ['expected', 'diff','data', 'actual'].every((name)=> args[0].indexOf(name)=== -1) ) {
             try {
                 const spl = args[0].split("\\");
 
                 const fileName = spl[spl.length-1];
 
-                saveFileRequest("https://localhost:3000", "example-project", "v3", fileName,args[1])
+                saveFileRequest("https://localhost:3001", "example-project", "v3", fileName,args[1])
             } catch (e) {
                 console.log(e)
             }
@@ -46,45 +46,34 @@ fetch('http://localhost:3000/screenshots/example-project/v3').then((res)=>res).t
             return orygFsWrite(...args)
         }
     }
+    const oryg = console.log;
+
+    (console.log as any) = (...args)=> {
+        if (args[0] && args[0].indexOf('is re-generated') > -1) {
+            const fileName = args[0].split("\\").at(-1)
+            oryg(`${fileName}`,...args.slice(1))
+        }else {
+            oryg(...args)
+        }
+    }
+
+    const orygFsMkDir = fs.mkdirSync;
+
+    (fs.mkdirSync as any) = (...args)=> {
+        if (args[0].indexOf('-snapshots') > -1){
+            return
+        }
+
+        return orygFsMkDir(...args)
+    }
 });
 
 
 export async function saveFileRequest(url: string,project: string,version: string,fileName:string, imgToUpload: Buffer) {
-    const res = await httpsPost( {
-        method: 'POST',
-        hostname:'localhost',
-        port: 3000,
-        path: `${url}/save/${project}/${version}/${fileName}`,
-        body: imgToUpload,
-        headers: {'Content-Type': 'application/octet-stream'}
-    });
-    if (res.ok) {
-        console.log("OK");
+    try {
+        await fetch(`${url}/save/${project}/${version}/${fileName}`, {method: 'POST', body:imgToUpload, headers: {'Content-Type': 'application/octet-stream'}})
+    } catch (e) {
+        console.error(e)
+        throw e
     }
-}
-
-function httpsPost({body, ...options}) {
-    return new Promise((resolve,reject) => {
-        const req = http.request({
-            method: 'POST',
-            ...options,
-        }, res => {
-            const chunks = [];
-            res.on('data', data => chunks.push(data))
-            res.on('end', () => {
-                let resBody = Buffer.concat(chunks);
-                switch(res.headers['content-type']) {
-                    case 'application/json':
-                        resBody = JSON.parse(resBody);
-                        break;
-                }
-                resolve(resBody)
-            })
-        })
-        req.on('error',reject);
-        if(body) {
-            req.write(body);
-        }
-        req.end();
-    })
 }
