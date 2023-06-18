e74020f32b7cc9f1983c1c1ac0b83e1e1c58920a
Pawel Fajfer <fajfer.pawel@gmail.com>
2023-06-18 14:49:53 +0200

    Small refactor of eyesight-playwright

--- packages/eyesight-playwright/src/index.ts
+++ packages/eyesight-playwright/src/index.ts
@@ -1,29 +1,34 @@
-import fs from "fs";
-import http from "http";
+import fs, {PathLike} from "fs";
 
-fetch('http://localhost:3001/screenshots/example-project/v3').then((res)=>res).then((r)=> r.json()).then((res)=> {
+fetch('http://localhost:3001/screenshots/example-project/v3').then((r)=> r.json()).then((res: Array<{ name:string, content: string }>)=> {
     const orygFs= fs.existsSync;
     const orygFsPromiseRead= fs.promises.readFile;
 
-    (fs.existsSync as any) = (...args:any[])=> {
-        if (args[0].endsWith('png')) {
+    (fs.existsSync as any) = (path: PathLike)=> {
+        if (typeof path === 'string' &&path.endsWith('png')) {
 
-            const spl = args[0].split("\\");
+            const spl = path.split("\\");
 
             const fileName = spl[spl.length-1];
             return res.map((re)=> re.name).includes(fileName)
         }
 
-        return orygFs(...args)
+        return orygFs(path)
     }
 
-    (fs.promises.readFile as any) = async (...args:any[])=> {
-        if (args[0].endsWith('png')) {
+    (fs.promises.readFile as any) = async (...args:Parameters<typeof fs.promises.readFile>)=> {
+        if (typeof args[0] === 'string' && args[0].endsWith('png')) {
 
             const spl = args[0].split("\\");
 
             const fileName = spl[spl.length-1];
-            return Buffer.from(res.find((re)=> re.name === fileName).content, 'base64')
+            const screenshotFromServer = res.find((re)=> re.name === fileName);
+
+            if (!screenshotFromServer) {
+                throw Error(`Screenshot with name ${fileName} not found on eyesight server.`)
+            }
+
+            return Buffer.from(screenshotFromServer.content, 'base64')
         }
 
         return orygFsPromiseRead(...args)
@@ -31,14 +36,13 @@ fetch('http://localhost:3001/screenshots/example-project/v3').then((res)=>res).t
 
     const orygFsWrite= fs.writeFileSync;
 
-    (fs.writeFileSync as any) = (...args:any[])=> {
-        if (args[0].endsWith('png') && ['expected', 'diff','data', 'actual'].every((name)=> args[0].indexOf(name)=== -1) ) {
+    (fs.writeFileSync as any) = (...args:Parameters<typeof fs.writeFileSync>)=> {
+        const path = args[0];
+        if (typeof path === 'string' && path.endsWith('png') && ['expected', 'diff','data', 'actual'].every((name)=> path.indexOf(name)=== -1) ) {
             try {
-                const spl = args[0].split("\\");
-
+                const spl = path.split("\\");
                 const fileName = spl[spl.length-1];
-
-                saveFileRequest("https://localhost:3001", "example-project", "v3", fileName,args[1])
+                saveFileRequest("http://localhost:3001", "example-project", "v3", fileName,args[1])
             } catch (e) {
                 console.log(e)
             }
@@ -46,21 +50,26 @@ fetch('http://localhost:3001/screenshots/example-project/v3').then((res)=>res).t
             return orygFsWrite(...args)
         }
     }
-    const oryg = console.log;
 
-    (console.log as any) = (...args)=> {
-        if (args[0] && args[0].indexOf('is re-generated') > -1) {
-            const fileName = args[0].split("\\").at(-1)
-            oryg(`${fileName}`,...args.slice(1))
+    const orygConsoleLog = console.log;
+
+    (console.log as any) = (...args: Parameters<typeof console.log>)=> {
+        const message = args[0];
+        if (message && message.indexOf('is re-generated') > -1) {
+            const fileName = message.split("\\").at(-1)
+            orygConsoleLog(`${fileName}`,...args.slice(1))
+        }else if(message && message.indexOf("A snapshot doesn't exist") > -1){
+            orygConsoleLog(`Error: A snapshot doesn't exist at eyesight server: version v3, project: example-project, filename: <filename> , writing actual.`)
         }else {
-            oryg(...args)
+            orygConsoleLog(...args)
         }
     }
 
     const orygFsMkDir = fs.mkdirSync;
 
-    (fs.mkdirSync as any) = (...args)=> {
-        if (args[0].indexOf('-snapshots') > -1){
+    (fs.mkdirSync as any) = (...args: Parameters<typeof fs.mkdirSync>)=> {
+
+        if (typeof args[0] === 'string' && args[0].indexOf('-snapshots') > -1){
             return
         }
 
@@ -69,7 +78,7 @@ fetch('http://localhost:3001/screenshots/example-project/v3').then((res)=>res).t
 });
 
 
-export async function saveFileRequest(url: string,project: string,version: string,fileName:string, imgToUpload: Buffer) {
+export async function saveFileRequest(url: string,project: string,version: string,fileName:string, imgToUpload: ArrayBufferView | string) {
     try {
         await fetch(`${url}/save/${project}/${version}/${fileName}`, {method: 'POST', body:imgToUpload, headers: {'Content-Type': 'application/octet-stream'}})
     } catch (e) {
